285f286a4bc6cc5fdc39fa5802aad4c0b1267627
Prilly Akwila Natanael <akwila.natanael@gmail.com>
2022-09-19 20:06:21 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:dicoding_news_app/models/article.dart';
+import 'package:dicoding_news_app/pages/ui/article_detail_page.dart';
+import 'package:dicoding_news_app/pages/ui/article_web_view.dart';
+import 'package:dicoding_news_app/pages/ui/news_list_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'News App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: NewsListPage.routeName,
+      routes: {
+        NewsListPage.routeName: (context) => const NewsListPage(),
+        ArticleDetailPage.routeName: (context) => ArticleDetailPage(
+              article: ModalRoute.of(context)?.settings.arguments as Article,
+            ),
+        ArticleWebView.routeName: (context) => ArticleWebView(
+              url: ModalRoute.of(context)?.settings.arguments as String,
+            ),
+      },
+    );
+  }
+}
---
+++ lib/pages/ui/article_detail_page.dart
@@ -0,0 +1,77 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:dicoding_news_app/models/article.dart';
+import 'package:dicoding_news_app/pages/ui/article_web_view.dart';
+import 'package:flutter/material.dart';
+
+class ArticleDetailPage extends StatelessWidget {
+  static const routeName = '/article_detail';
+  final Article article;
+  const ArticleDetailPage({Key? key, required this.article}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(article.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(article.urlToImage),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(article.description),
+                  Divider(
+                    color: Colors.grey,
+                  ),
+                  Text(
+                    article.title,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.grey,
+                  ),
+                  Text('Date: ${article.publishedAt}'),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text('Author: ${article.author}'),
+                  Divider(
+                    color: Colors.grey,
+                  ),
+                  Text(
+                    article.content,
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(
+                        context,
+                        ArticleWebView.routeName,
+                        arguments: article.url,
+                      );
+                    },
+                    child: Text('Read more'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
